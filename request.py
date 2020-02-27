a947c26790799535bf154674400b22854d015f1d
lkosigi <45931586+lkosigi@users.noreply.github.com>
2020-02-27 10:38:22 +0530

    Add files via upload

--- request.py
+++ request.py
@@ -1,6 +1,5 @@
 import requests
 
-url = 'http://localhost:5000/predict_api'
-r = requests.post(url,json={'experience':2, 'test_score':9, 'interview_score':6})
-
+url = 'http://localhost:5000/data-science-intern-api'
+r = requests.post(url,json={'Python (out of 3)':2, 'R Programming (out of 3)':1, 'Data Science (out of 3)':3, 'type_of_degree_PG':0,'type_of_degree_UG':1,'ML':1,'DL':1,'NLP':1,'SM':0,'AWS':0,'SQL':0,'NoSQL':0,'Excel':0,'year':2})
 print(r.json())
